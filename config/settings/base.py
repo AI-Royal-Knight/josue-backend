c78f3b1cce916e80c6bd0ef609bf892bfc881051
Ashiqul Islam Ayon <ashiqulislamayon28@gmail.com>
2026-08-08 10:08:01 +0600

    feat: relocate sqlite database file to a dedicated directory within the project root

--- config/settings/base.py
+++ config/settings/base.py
@@ -120,10 +120,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+DB_DIR = BASE_DIR / 'sqlite_data'
+DB_DIR.mkdir(exist_ok=True)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DB_DIR / 'db.sqlite3',
     }
 }
 
